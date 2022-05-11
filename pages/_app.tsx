8a92b914ac630812ecb314a86161ae8066e37475
Hamim <rahmanhamiminfo@gmail.com>
2022-05-11 13:03:27 +0600

    edx initialized

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,42 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#fff",
+      main: "#00262B",
+      dark: "#000",
+    },
+    secondary: {
+      light: "#99241c",
+      main: "#D13328",
+      dark: "#000",
+    },
+  },
+  components: {
+    MuiButton: {
+      variants: [
+        {
+          props: { variant: "contained" },
+          style: {
+            borderRadius: "1px",
+          },
+        },
+      ],
+    },
+  },
+});
+
+console.log(theme);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
