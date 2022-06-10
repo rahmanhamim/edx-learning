9b829736a38f7a928dff9f3c2c11c6fb14ab8438
hamim <rahmanhamiminfo@gmail.com>
2022-06-11 01:18:51 +0600

    h

--- src/components/course/CourseAsideContent.tsx
+++ src/components/course/CourseAsideContent.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CourseAsideDates from "./CourseAsideDates";
 import CourseAsideDiscount from "./CourseAsideDiscount";
 import CourseAsideGoalCard from "./CourseAsideGoalCard";
